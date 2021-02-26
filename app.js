c41cb2af4bc9f28475ac17e4a0c9b0ea37d6fa3b
Richie Roman <answer417@gmail.com>
2021-02-26 14:44:19 -0500

    adding templates

--- app.js
+++ app.js
@@ -4,177 +4,176 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const chalk = require('chalk');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let employeeArr = [];
+const team = [];
 
-const initialQuestion = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What type of employee?',
-            name: 'employeeType',
-            choices: [Manager, Engineer, Intern]
-        },
-    ])
-        .then(answer => {
 
-            if (answer.employeeType === 'Manager') {
-                managerQuestions();
-            } else if
-               (answer.employeeType === 'Engineer') {
-                engineerQuestions();
-            } else if
-               (answer.employeeType === 'Intern') {
-                internQuestions();
-            }
-            else {
-                console.log('Done!');
-                return;
-            }
-        })
+const addManager = () => {
+    return new Promise((res) => {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter manager's name:",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's ID:",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's email:",
+                    name: "email",
+                    
+                    default: () => {},
+                    validate: function (email) {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                     if (valid) {
+                    return true;
+                    } else {
+                    console.log(" ---Please enter a valid email!---")
+                    return false;
+                    }
+                  }  
+                },
+                {
+                    type: "input",
+                    message: "Enter manager's office number:",
+                    name: "officeNumber",
+                },
+                
+            ]).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                team.push(manager);
+                res();
+            });
+    });
 }
 
-initialQuestion();
-
-const internQuestions = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the interns name?',
-            name: 'internName'
-        },
-        {
-            type: 'input',
-            message: 'What is the interns employee id?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is the interns email?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the interns school?',
-            name: 'school',
-        },
-        {
-            type: 'confirm',
-            message: 'Do you need to enter another employee?',
-            name: 'addCheck',
-        },
-    ])
-        .then(answers => {
-            const intern = new Intern(answers.internName, answers.id, answers.email, answers.school);
-            employeeArr.push(intern);
-            console.log(employeeArr);
-
-            if (answers.addCheck) {
-                initialQuestion();
+const addEmployee = () => {
+    return new Promise((resolve) => {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Select the Employee you would like to add:",
+                name: "role",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    {
+                        name: "No more employees to add",
+                        value: false
+                    }
+                ]
+            },
+            {
+                message: "Enter engineer's name:",
+                name: "name",
+                when: ({ role }) => role === "Engineer"
+            },
+            {
+                message: "Enter intern's name:",
+                name: "name",
+                when: ({ role }) => role === "Intern"
+            },
+            {
+                message: "Enter engineer's ID:",
+                name: "id",
+                when: ({ role }) => role === "Engineer"
+            },
+            {
+                message: "Enter intern's ID:",
+                name: "id",
+                when: ({ role }) => role === "Intern"
+            },
+            {
+                message: "Enter engineer's email address:",
+                name: "email",
+                
+                default: () => {},
+                    validate: function (email) {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                     if (valid) {
+                    return true;
+                    } else {
+                    console.log(" ---Please enter a valid email!---")
+                    return false;
+                    }
+                  },  
+                when: ({ role }) => role === "Engineer"
+            },
+            {
+                message: "Enter intern's email address:",
+                name: "email",
+                
+                default: () => {},
+                    validate: function (email) {
+                    valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                     if (valid) {
+                    return true;
+                    } else {
+                    console.log(" ---Please enter a valid email!---")
+                    return false;
+                    }
+                  }, 
+                when: ({ role }) => role === "Intern"
+            },
+            {
+                message: "Enter engineer's GitHub username:",
+                name: "github",
+                when: ({ role }) => role === "Engineer"
+            },
+            {
+                message: "Enter Intern's School name:",
+                name: "school",
+                when: ({ role }) => role === "Intern"
+            }
+            
+        ]).then(answers => {
+            if (answers.role) {
+                switch (answers.role) {
+                    case "Engineer":
+                        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+                        team.push(engineer);
+                        break;
+                    case "Intern":
+                        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+                        team.push(intern);
+                        break;
+                }
+                return addEmployee().then(() => resolve());
             } else {
-                let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
-                    if (err) throw err;
-                    console.log(chalk.green('The file has been saved!'));
-                });
+                return resolve();
             }
         })
+    })
 }
 
-const engineerQuestions = () => {
-    inquirer.prompt([
 
-        {
-            type: 'input',
-            message: 'What is the engineers name?',
-            name: 'engineersName'
-        },
-        {
-            type: 'input',
-            message: 'What is the engineers employee id?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is the engineers email?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the engineers github username?',
-            name: 'github',
-        },
-        {
-            type: 'confirm',
-            message: 'Do you need to enter another employee?',
-            name: 'addCheck',
-        },
-    ])
-        .then(answers  => {
-            const engineer = new Engineer(answers.engineerName, answers.id, answers.email, answers.github);
-            employeeArr.push(engineer);
-            console.log(employeeArr);
+addManager().then(() => {
+    return addEmployee();
+ 
+}).then(() => {
+    const templateHTML = render(team)
+    generatePage(templateHTML);
+}).catch((err) => {
+    console.log(err);
+});
 
-            if(answers.addCheck) {
-                initialQuestion();
-            } else {
-                let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
-                    if (err) throw err;
-                    console.log(chalk.green('The file has been saved!'));
-                })
-            }
-        })
-}
 
-const managerQuestions = () => {
-    inquirer.prompt([
+const generatePage = (htmlPage) => {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
 
-        {
-            type: 'input',
-            message: 'What is the managers name?',
-            name: 'managerName'
-        },
-        {
-            type: 'input',
-            message: 'What is the managers id?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is the managers email?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the managers office number?',
-            name: 'officeNumber',
-        },
-        {
-            type: 'confirm',
-            message: 'Do you need to enter another employee?',
-            name: 'addCheck',
-        },
-    ])
-        .then(answers => {
-            const manager = new Manager(answers.managerName, answers.id, answers.email, answers.officeNumber);
-            employeeArr.push(manager);
-            console.log(employeeArr);
-
-            if (answers.addCheck) {
-                initialQuestion();
-            } else {
-                let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
-                    if (err) throw err;
-                    console.log(chalk.green('The file has been saved!'));
-                });
-            }
-        })
+    fs.writeFile(outputPath, htmlPage, "utf-8", (err) => {
+        if(err) throw err;
+        console.log("Team profile page generated!");
+    });
 }
