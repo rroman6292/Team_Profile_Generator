954e9447d532e4d47d0afa3d6aeeb5555d900755
Richard Roman <answer417@gmail.com>
2021-02-27 11:53:03 -0500

    Update Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -8,7 +8,7 @@ test("Can set school via constructor", () => {
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Foo", 1, "test@test.com", "UCF");
   expect(e.getRole()).toBe(testValue);
 });
 
